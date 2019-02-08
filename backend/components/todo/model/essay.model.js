f607e55ef7ffe1a8de19df2f68429636c8c2999f
tal8374 <talivanov@gmail.com>
2019-02-08 16:06:48 +0200

    fixing essay schema.

--- backend/components/todo/model/essay.model.js
+++ backend/components/todo/model/essay.model.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 
 const EssaySchema = new Schema({
     body: {type: 'String', required: true},
-    remarks: {type: 'String', required: false},
-    grade: {type: 'Number', required: false},
+    remarks: {type: 'String', required: false}
 });
 
 module.exports = EssaySchema;
