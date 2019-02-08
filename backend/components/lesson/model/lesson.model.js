09f4800bac1a04f8270d3eb0bc03733a961e5779
tal8374 <talivanov@gmail.com>
2019-02-08 15:38:25 +0200

    Creating lesson schema.

---
+++ backend/components/lesson/model/lesson.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const UserSchema = require('../../user/model/user.model');
+const CommentSchema = require('../../comment/model/comment.model');
+const LocationSchema = require('../../location/model/location.model');
+const TodoSchema = require('../../todo/model/todo.model');
+const CancellationSchema = require('../../lesson/model/cancellation.model');
+
+const LessonSchema = new Schema({
+    teacher: {type: UserSchema, required: true},
+    student: {type: UserSchema, required: true},
+    comment: {type: CommentSchema, required: false},
+    location: {type: LocationSchema, required: false},
+    cancellation: {type: CancellationSchema, required: false},
+    todo: {type: TodoSchema, required: false},
+    date: {type: 'Date', required: true},
+    createdAt: {type: 'Date', required: true, default: new Date()},
+    price: {type: 'Number', required: false},
+    type: {type: 'String', required: false}
+});
+
+module.exports = mongoose.model('lesson', LessonSchema);
