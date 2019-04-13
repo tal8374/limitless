8f5f96981b393934a75ebffbba0b2f7f2029f2ee
tal8374 <talivanov@gmail.com>
2019-04-13 12:46:51 +0300

    Creating profile from user data

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -5,58 +5,97 @@ import Rating from "react-rating";
 import {withRouter} from "react-router-dom";
 
 class Profile extends Component {
+    goToProfile() {
+        let path = '/profile/' + this.props.user.firstName;
+        this.props.history.push(path);
+    }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            user: props.user ? props.user : {
-                key: '12',
-                firstName: '12',
-                lastName: 'last',
-                registeredAt: new Date,
-                phone: '0543534542',
-                email: 'email@gmail.com',
-                roles: ['teacher'],
-                dateOfBirth: new Date(),
-                photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf3lIy5jnenOOIroabuRLT6i_DjzJoxU9mKSA8r-jKwtfQWanu',
-                from: {
-                    country: 'country',
-                    city: 'city',
-                    street: 'street',
-                }
-            },
-            profile: props.profile
+    getPhoto() {
+        if (this.props.user.photo) {
+            return <Item.Image src={this.props.user.photo}/>
+        } else {
+            return <Item.Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
         }
     }
 
-    goToProfile() {
-        let path = '/profile/' + this.state.user.firstName;
-        this.props.history.push(path);
+    getFullName() {
+        if (this.props.user.firstName) {
+            let lastName = this.props.user.lastName ? this.props.user.lastName : '';
+
+            return <Item.Header>{this.props.user.firstName + ' ' + lastName}</Item.Header>
+        } else {
+            let email = this.props.user.email;
+            let emailTitle = email.substring(0, email.indexOf('@'));
+
+            return <Item.Header>{emailTitle}</Item.Header>
+        }
+    }
+
+    formatDate(date) {
+        var monthNames = [
+            "January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"
+        ];
+
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+
+        return day + ' ' + monthNames[monthIndex] + ' ' + year;
+    }
+
+    getNumberOfStudents() {
+        if (this.props.user.roles.includes('teacher')) {
+            return <Item.Meta>Number of students: {this.props.user.students.length}</Item.Meta>
+        } else {
+            return null;
+        }
+    }
+
+    getNumberOfTeachers() {
+        if (this.props.user.roles.includes('student')) {
+            return <Item.Meta>Number of teachers: {this.props.user.teachers.length}</Item.Meta>
+        } else {
+            return null;
+        }
+    }
+
+    getDescription() {
+        if (this.props.user.description) {
+            return <Item.Description>
+                {this.props.user.description}
+            </Item.Description>
+        } else {
+            return null;
+        }
+    }
+
+    getRating() {
+        return <Item.Meta>
+            <Rating
+                style={{color: '#FFA500'}}
+                emptySymbol="fa fa-star-o fa-2x"
+                fullSymbol="fa fa-star checked fa-2x"
+                initialRating={3}
+                readonly
+            />
+        </Item.Meta>
     }
 
     render() {
         return (
             <Item.Group divided onClick={this.goToProfile.bind(this)}>
                 <Item>
-                    <Item.Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-
+                    {this.getPhoto()}
                     <Item.Content>
-                        <Item.Header>{this.state.user.firstName}</Item.Header>
-                        <Item.Meta>Last active: {new Date().toDateString()}</Item.Meta>
-                        <Item.Meta>Number of students: 6</Item.Meta>
-                        <Item.Meta>Number of lessons: 6</Item.Meta>
-                        <Item.Meta>
-                            <Rating
-                                style={{color: '#FFA500'}}
-                                emptySymbol="fa fa-star-o fa-2x"
-                                fullSymbol="fa fa-star checked fa-2x"
-                                initialRating={3}
-                                readonly
-                            />
-                        </Item.Meta>
-                        <Item.Description>
-                            some description
-                        </Item.Description>
+                        {this.getFullName()}
+                        <Item.Meta>Last active: {this.formatDate(new Date(this.props.user.lastActive))}</Item.Meta>
+                        {this.getNumberOfStudents()}
+                        {this.getNumberOfTeachers()}
+                        {this.getRating()}
+                        {this.getDescription()}
                         <Item.Extra>
                             <Button
                                 color="grey">
