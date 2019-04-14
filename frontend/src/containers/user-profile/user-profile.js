dae802fdae42b4808cb1987e595b386ac0a193f6
tal8374 <talivanov@gmail.com>
2019-04-14 23:21:00 +0300

    Fetching the user from the reducer

--- frontend/src/containers/user-profile/user-profile.js
+++ frontend/src/containers/user-profile/user-profile.js
@@ -5,6 +5,7 @@ import Profile from "../../components/profile/profile";
 import Calendar from "../../components/account/calendar/calendar";
 import {fetchUser} from "../../actions";
 import {connect} from "react-redux";
+import StatusMessage from "../teachers/teachers";
 
 class UserProfileContainer extends Component {
 
@@ -35,16 +36,34 @@ class UserProfileContainer extends Component {
     }
 
     render() {
+        const {isLoading, error, user} = this.props;
+
+        if (error || !user || isLoading || user.length === 0) {
+            return (
+                <StatusMessage
+                    error={error || !user}
+                    errorClassName="users-error"
+                    errorMessage={error}
+                    loading={isLoading}
+                    loadingMessage={`We are fetching the user for you`}
+                    nothing={user && user.length === 0}
+                    nothingMessage={`No user to display`}
+                    nothingClassName="users-error"
+                    type="default"
+                />
+            );
+        }
+
         return (
             <Segment>
                 <Grid style={{marginTop: '1%'}}>
                     <Grid.Row>
                         <Grid.Column width={1}/>
                         <Grid.Column width={11}>
-                            <Profile { ...this.props }/>
+                            <Profile {...this.props}/>
                         </Grid.Column>
                         <Grid.Column width={3}>
-                            <Comments { ...this.props }/>
+                            <Comments comments={this.props.user.comments}/>
                         </Grid.Column>
                         <Grid.Column width={1}/>
                     </Grid.Row>
