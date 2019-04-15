919d5c7fd98d3501c9fbc707974de46f644f0b4f
tal8374 <talivanov@gmail.com>
2019-04-15 17:18:14 +0300

    Showing to user only relevant options

--- frontend/src/components/account/account-menu/account-menu.js
+++ frontend/src/components/account/account-menu/account-menu.js
@@ -7,6 +7,7 @@ import Teachers from '../teachers/teachers';
 import Students from '../students/students';
 import Setting from '../setting/setting';
 import UserProfile from "../../user-profile/user-profile";
+import StatusMessage from "../../status-message/status-message";
 
 class AccountMenu extends Component {
     state = {activeItem: 'setting'};
@@ -31,21 +32,44 @@ class AccountMenu extends Component {
     render() {
         const {activeItem} = this.state;
 
+        let {isLoading, error, loggedInUser} = this.props;
+
+        if (error || !loggedInUser || isLoading) {
+            return (
+                <StatusMessage
+                    error={error || !loggedInUser}
+                    errorClassName="users-error"
+                    errorMessage={error}
+                    loading={isLoading}
+                    loadingMessage={`We are fetching the setting details for you`}
+                    nothing={loggedInUser}
+                    nothingMessage={`No setting details to display`}
+                    nothingClassName="users-error"
+                    type="default"
+                />
+            );
+        }
+
         return (
             <Grid>
                 <Grid.Column width={2}>
                     <Menu fluid vertical tabular>
                         <Menu.Item name='profile' active={activeItem === 'profile'} onClick={this.handleItemClick}/>
                         <Menu.Item name='calendar' active={activeItem === 'calendar'} onClick={this.handleItemClick}/>
-                        <Menu.Item name='teachers' active={activeItem === 'teachers'} onClick={this.handleItemClick}/>
-                        <Menu.Item name='students' active={activeItem === 'students'} onClick={this.handleItemClick}/>
+                        {loggedInUser.roles.includes('student') ?
+                            <Menu.Item name='teachers' active={activeItem === 'teachers'}
+                                       onClick={this.handleItemClick}/> : null}
+                        {loggedInUser.roles.includes('teacher') ?
+                            <Menu.Item name='students' active={activeItem === 'students'}
+                                       onClick={this.handleItemClick}/>: null}
+
                         <Menu.Item name='setting' active={activeItem === 'setting'} onClick={this.handleItemClick}/>
                     </Menu>
                 </Grid.Column>
 
                 <Grid.Column stretched width={14}>
                     <Segment>
-                        { this.getSegment() }
+                        {this.getSegment()}
                     </Segment>
                 </Grid.Column>
             </Grid>
