fffa08c93ee5069c9513d12316e321418d278f9f
tal8374 <talivanov@gmail.com>
2019-04-21 14:27:27 +0300

    .

--- frontend/src/containers/messages/messages.js
+++ frontend/src/containers/messages/messages.js
@@ -2,40 +2,62 @@ import React, {Component} from 'react';
 import ConversationList from "../../components/messages/conversation-list/conversation-list";
 import MessageList from "../../components/messages/message-list/message-list";
 import './messages.css';
+import {fetchTeachers} from "../../actions";
+import {connect} from "react-redux";
+import StatusMessage from "../../components/status-message/status-message";
 
 class MessagesContainer extends Component {
     render() {
+        let {loggedInUser} = this.props;
+
+        if(!loggedInUser) {
+            return <StatusMessage
+                // error={error || !teachers}
+                // errorClassName="users-error"
+                // errorMessage={error}
+                // loading={isLoading}
+                // loadingMessage={`We are fetching the teachers for you`}
+                nothing={!loggedInUser || loggedInUser.message.length === 0}
+                nothingMessage={`No messages to display`}
+                nothingClassName="users-error"
+                type="modal"
+            />
+        }
+
+        console.log(loggedInUser)
+
         return (
             <div className="messenger">
-                {/* <Toolbar
-          title="Messenger"
-          leftItems={[
-            <ToolbarButton key="cog" icon="ion-ios-cog" />
-          ]}
-          rightItems={[
-            <ToolbarButton key="add" icon="ion-ios-add-circle-outline" />
-          ]}
-        /> */}
-
-                {/* <Toolbar
-          title="Conversation Title"
-          rightItems={[
-            <ToolbarButton key="info" icon="ion-ios-information-circle-outline" />,
-            <ToolbarButton key="video" icon="ion-ios-videocam" />,
-            <ToolbarButton key="phone" icon="ion-ios-call" />
-          ]}
-        /> */}
-
                 <div className="scrollable sidebar">
-                    <ConversationList />
+                    <ConversationList
+                        {...this.props}
+                    />
                 </div>
 
                 <div className="scrollable content">
-                    <MessageList />
+                    <MessageList
+                        {...this.props}
+                    />
                 </div>
             </div>
         );
     }
 }
 
-export default MessagesContainer;
+const mapStateToProps = state => ({
+    // isLoading: state.messages.isLoading,
+    // messages: state.messages.messages,
+    // error: state.messages.error,
+    loggedInUser: state.auth.loggedInUser,
+});
+
+const mapDispatchToProps = dispatch => ({
+    // fetchMessages: () => {
+    //     dispatch(fetchMessages());
+    // },
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(MessagesContainer);
